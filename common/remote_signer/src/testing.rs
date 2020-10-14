2729bb24135e123bd84665f0a3d11910acfe7193
Herman Junge <hermanjunge@protonmail.com>
2020-10-14 05:31:04 +0100

    Documenting failed test strategy TODO

--- common/remote_signer/src/testing.rs
+++ common/remote_signer/src/testing.rs
@@ -201,7 +201,7 @@ mod randao {
 
         // TODO
         // Test that the data field is actually empty!
-        // i.e. the hack works.
+        // i.e. that this DummyRandao hack (only for tests) works.
 
         let signature = do_sign_request(&test_client, &test_input);
 
@@ -211,16 +211,51 @@ mod randao {
     }
 }
 
-// TODO
-// - Test from_components with an invalid url.
-// - We will hunt for errors that we are not having
-//   using the conventional sign API, and
-//   test them here.
-// - Check that I am building the right json for the three cases.
-// - Tests cases the remote signer accounts for:
-//   missing field signingRoot (400)
-//   bad request
-//   etc
-// - Another silly idea:
-//   Let's do the very test agains the "real" `.sign()` functions
-// - Test that serde is effectively NOT sending data with None.
+// # Test Strategy (TODO)
+//
+// ## JSON serialization
+// * block
+// * attestation
+// * None when is randao
+//
+// ## Remote signer vs The LH "local" implementation
+// * block
+// * attestation
+// * randao
+//
+// ## Message preparation
+// * Somebody sends a Domain type X, but data from a different type, or none (if it applies)
+// * public_key field is empty
+// * no_public_key_in_path (5 cases)
+// * unsupported bls_domain
+// * data: People implementing a new RemoteSignerObject: SignedRoot + Serialize
+//   * what happens? Should pass? no?
+// * bad fork field (establish what can make this a bad parameter)
+// * bad epoch field (establish what can make this a bad parameter)
+// * bad genesis validators root field (establish what can make this a bad parameter)
+// * bad spec (establish what can make this a bad parameter)
+//
+// ## Errors that the remote signer shoould catch, but we don't trust and check anyways
+// * additional_path_segments (3 cases)
+// * invalid_public_key (6 cases)
+// * invalid json (4 cases)
+// * signing_root_in_json_not_a_string (4 cases)
+//
+// ## POST
+// * Server unavailable / off
+// * Server error (do the classic problem with the directories)
+// * Malformed URL
+// * Bad URL (to get 404s)
+// * Bad response (use mock)
+//   * no json
+//   * missing_signing_root_in_json
+//   * json, empty_signing_root_in_json#
+//   * invalid_string_signing_root
+//   * json, signature, but extra fields (should be ignored?)
+// * secret key problems
+//   * key_not_found
+//   * invalid_secret_key
+//   * key mismatch
+// * Weird status code (418)
+// * Timeout
+//
